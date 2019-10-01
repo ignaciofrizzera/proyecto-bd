60747b527bc10b287a8c06fd2bdfa690d9ab2656
DESKTOP-UHIF4O8\Sandra <ignaciofrizzera@gmail.com>
2019-10-01 14:59:49 -0300

    Genero el repo y cargo los scripts que se utilizaron para crear el esquema de la BD e inicializarla con información.

---
+++ scripts/datos.sql
@@ -0,0 +1,119 @@
+
+#Carga de datos de prueba
+
+use vuelos;
+
+#INSERCION EN MODELOS AVION
+INSERT INTO modelos_avion(modelo, fabricante, cabinas, cant_asientos) VALUES("Boeing 0","Boeing.Co",2,50);
+INSERT INTO modelos_avion(modelo, fabricante, cabinas, cant_asientos) VALUES("Boeing 1","Boeing.Co",3,75);
+INSERT INTO modelos_avion(modelo, fabricante, cabinas, cant_asientos) VALUES("Boeing 2","Boeing.Co",4,100);
+INSERT INTO modelos_avion(modelo, fabricante, cabinas, cant_asientos) VALUES("Boeing 3","Boeing.Co",6,140);
+INSERT INTO modelos_avion(modelo, fabricante, cabinas, cant_asientos) VALUES("Delta 0","Delta.Co",2,100);
+
+
+#INSERCION EN COMODIDADES
+INSERT INTO comodidades(codigo, descripcion) VALUES(1, "Asiento mas grande");
+INSERT INTO comodidades(codigo, descripcion) VALUES(2, "Asiento con masajeador");
+INSERT INTO comodidades(codigo, descripcion) VALUES(3, "Asiento plegable");
+INSERT INTO comodidades(codigo, descripcion) VALUES(4, "Televisor 62 pulgadas");
+INSERT INTO comodidades(codigo, descripcion) VALUES(5, "Asiento con calentador de espalda");
+
+
+#INSERCION EN CLASES
+INSERT INTO clases(nombre, porcentaje) VALUES("Economica", 0.65);
+INSERT INTO clases(nombre, porcentaje) VALUES("Primera clase", 0.3);
+INSERT INTO clases(nombre, porcentaje) VALUES("Media", 0.5);
+INSERT INTO clases(nombre, porcentaje) VALUES("Super economica", 0.8);
+INSERT INTO clases(nombre, porcentaje) VALUES("Intermedia", 0.72);
+
+
+#INSERCION EN POSEE
+INSERT INTO posee(clase, comodidad) VALUES("Economica", 4);
+INSERT INTO posee(clase, comodidad) VALUES("Primera clase", 2);
+INSERT INTO posee(clase, comodidad) VALUES("Media", 5);
+INSERT INTO posee(clase, comodidad) VALUES("Super economica", 3);
+INSERT INTO posee(clase, comodidad) VALUES("Intermedia", 1);
+
+
+#INSERCION EN UBICACIONES
+INSERT INTO ubicaciones(pais, estado, ciudad, huso) VALUES("Argentina", "Buenos Aires", "Bahia Blanca", 6);
+INSERT INTO ubicaciones(pais, estado, ciudad, huso) VALUES("Argentina", "Buenos Aires", "La plata", 6);
+INSERT INTO ubicaciones(pais, estado, ciudad, huso) VALUES("Argentina", "Buenos Aires", "Mar del Plata", 6);
+INSERT INTO ubicaciones(pais, estado, ciudad, huso) VALUES("Argentina", "La Pampa", "Alpachiri", 6);
+INSERT INTO ubicaciones(pais, estado, ciudad, huso) VALUES("Argentina", "Buenos Aires", "Coronel Pringles", 6);
+
+
+#INSERCION EN AEROPUERTOS
+INSERT INTO aeropuertos(codigo, nombre, telefono, direccion, pais, estado, ciudad) VALUES("LP001", "Aeropuerto de La Plata", "0115918", "Av. 25 de Mayo", "Argentina", "Buenos Aires", "La Plata");
+INSERT INTO aeropuertos(codigo, nombre, telefono, direccion, pais, estado, ciudad) VALUES("BB002", "Aeropuerto de Bahia Blanca", "0115910", "Av. Libertadores", "Argentina", "Buenos Aires", "Bahia Blanca");
+INSERT INTO aeropuertos(codigo, nombre, telefono, direccion, pais, estado, ciudad) VALUES("MP003", "Aeropuerto de Mar del Plata", "011591", "Av. Corrientes", "Argentina", "Buenos Aires", "Mar del Plata");
+INSERT INTO aeropuertos(codigo, nombre, telefono, direccion, pais, estado, ciudad) VALUES("A004", "Aeropuerto de Alpachiri", "019124", "Suarez 520", "Argentina", "La Pampa", "Alpachiri");
+INSERT INTO aeropuertos(codigo, nombre, telefono, direccion, pais, estado, ciudad) VALUES("CP005", "Aeropuerto de Coronel Pringles", "011912", "Dorrego 915", "Argentina", "Buenos Aires", "Coronel Pringles");
+
+
+#INSERCION EN VUELOS PROGRAMADOS
+INSERT INTO vuelos_programados(numero, aeropuerto_salida, aeropuerto_llegada) VALUES("0001", "LP001", "CP005");
+INSERT INTO vuelos_programados(numero, aeropuerto_salida, aeropuerto_llegada) VALUES("0002", "CP005", "LP001");
+INSERT INTO vuelos_programados(numero, aeropuerto_salida, aeropuerto_llegada) VALUES("0003", "A004", "MP003");
+INSERT INTO vuelos_programados(numero, aeropuerto_salida, aeropuerto_llegada) VALUES("0004", "BB002", "LP001");
+INSERT INTO vuelos_programados(numero, aeropuerto_salida, aeropuerto_llegada) VALUES("0005", "MP003", "LP001");
+
+
+#INSERCION EN SALIDAS
+INSERT INTO salidas(vuelo, dia, hora_sale, hora_llega, modelo_avion) VALUES("0001", "Lu", '09:15', '12:45', "Boeing 0");
+INSERT INTO salidas(vuelo, dia, hora_sale, hora_llega, modelo_avion) VALUES("0002", "Do", '21:00', '06:30', "Boeing 1");
+INSERT INTO salidas(vuelo, dia, hora_sale, hora_llega, modelo_avion) VALUES("0003", "Ma", '14:20', '19:30', "Boeing 2");
+INSERT INTO salidas(vuelo, dia, hora_sale, hora_llega, modelo_avion) VALUES("0004", "Mi", '16:20', '22:45', "Boeing 3");
+INSERT INTO salidas(vuelo, dia, hora_sale, hora_llega, modelo_avion) VALUES("0005", "Vi", '05:35', '11:15', "Delta 0");
+
+
+#INSERCION EN BRINDA
+INSERT INTO brinda(vuelo, dia, clase, precio, cant_asientos) VALUES("0001", "Lu", "Economica", 8905.94, 100);
+INSERT INTO brinda(vuelo, dia, clase, precio, cant_asientos) VALUES("0001", "Lu", "Media", 10000, 10);
+INSERT INTO brinda(vuelo, dia, clase, precio, cant_asientos) VALUES("0002", "Do", "Primera Clase", 18905.94, 40);
+INSERT INTO brinda(vuelo, dia, clase, precio, cant_asientos) VALUES("0003", "Ma", "Media", 12905.94, 70);
+INSERT INTO brinda(vuelo, dia, clase, precio, cant_asientos) VALUES("0004", "Mi", "Super economica", 3905.94, 140);
+INSERT INTO brinda(vuelo, dia, clase, precio, cant_asientos) VALUES("0005", "Vi", "Intermedia", 6905.94, 75);
+
+
+#INSERCION EN INSTANCIAS VUELO
+INSERT INTO instancias_vuelo(vuelo, fecha, dia, estado) VALUES("0001", "2019/08/21", "Lu", "Cancelado");
+INSERT INTO instancias_vuelo(vuelo, fecha, dia, estado) VALUES("0002", "2019/11/07", "Do", "Demorado");
+INSERT INTO instancias_vuelo(vuelo, fecha, dia, estado) VALUES("0003", "2019/10/19", "Ma", "En horario");
+INSERT INTO instancias_vuelo(vuelo, fecha, dia, estado) VALUES("0004", "2019/12/31", "Mi", "Demorado");
+INSERT INTO instancias_vuelo(vuelo, fecha, dia, estado) VALUES("0005", "2020/02/01", "Vi", "En horario");
+
+
+#INSERCION EN EMPLEADOS
+INSERT INTO empleados(legajo, password, doc_tipo, doc_nro, apellido, nombre, direccion, telefono) VALUES(117815, "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "Tarjeta", 1001, "Gomez", "Juan", "Suarez 100", "466061");
+INSERT INTO empleados(legajo, password, doc_tipo, doc_nro, apellido, nombre, direccion, telefono) VALUES(117825, "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "Tarjeta", 1002, "Perez", "Cruz", "Brandsen 101", "447819");
+INSERT INTO empleados(legajo, password, doc_tipo, doc_nro, apellido, nombre, direccion, telefono) VALUES(117885, "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "Libreta", 1003, "Garcia", "Blas", "Alem 98", "445678");
+INSERT INTO empleados(legajo, password, doc_tipo, doc_nro, apellido, nombre, direccion, telefono) VALUES(117889, "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "Tarjeta", 1004, "Perez", "Pablo", "Colon 1", "901234");
+INSERT INTO empleados(legajo, password, doc_tipo, doc_nro, apellido, nombre, direccion, telefono) VALUES(117890, "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "Tarjeta", 1005, "Alem", "Ciro", "Colon 95", "121212");
+
+
+#INSERCION EN PASAJEROS
+INSERT INTO pasajeros(doc_tipo, doc_nro, apellido, nombre, direccion, telefono, nacionalidad) VALUES("Tarjeta", 101, "Frizzera", "Ignacio", "Caronti 100", "474747", "Argentina");
+INSERT INTO pasajeros(doc_tipo, doc_nro, apellido, nombre, direccion, telefono, nacionalidad) VALUES("Libreta", 102, "Chia", "Guido Giuliano", "San Martin 1001", "484848", "Argentina");
+INSERT INTO pasajeros(doc_tipo, doc_nro, apellido, nombre, direccion, telefono, nacionalidad) VALUES("Tarjeta", 103, "Hammilton", "Luis", "Francia 101", "494949", "Francia");
+INSERT INTO pasajeros(doc_tipo, doc_nro, apellido, nombre, direccion, telefono, nacionalidad) VALUES("Libreta", 104, "Perez", "Roberto", "Cabrera 69", "505050", "Noruega");
+INSERT INTO pasajeros(doc_tipo, doc_nro, apellido, nombre, direccion, telefono, nacionalidad) VALUES("Libreta", 105, "Lopez", "Raul", "Dorrego 1410", "515151", "Australia");
+
+
+#INSERCION EN RESERVAS
+INSERT INTO reservas(numero, fecha, vencimiento, estado, doc_tipo, doc_nro, legajo) VALUES(501, "2019/08/21", "2019/08/29", "Activa", "Tarjeta", 101, 117815);
+INSERT INTO reservas(numero, fecha, vencimiento, estado, doc_tipo, doc_nro, legajo) VALUES(502, "2019/10/21", "2019/10/30", "Activa", "Libreta", 102, 117815);
+INSERT INTO reservas(numero, fecha, vencimiento, estado, doc_tipo, doc_nro, legajo) VALUES(503, "2019/04/21", "2019/08/11", "Activa", "Libreta", 105, 117890);
+INSERT INTO reservas(numero, fecha, vencimiento, estado, doc_tipo, doc_nro, legajo) VALUES(504, "2019/11/21", "2020/02/26", "Activa", "Tarjeta", 103, 117825);
+INSERT INTO reservas(numero, fecha, vencimiento, estado, doc_tipo, doc_nro, legajo) VALUES(505, "2019/01/21", "2019/11/21", "Activa", "Libreta", 104, 117889);
+INSERT INTO reservas(numero, fecha, vencimiento, estado, doc_tipo, doc_nro, legajo) VALUES(506, "2019/08/21", "2019/08/29", "Activa", "Libreta", 105, 117815);
+
+
+#INSERCION EN RESERVA VUELO CLASE
+INSERT INTO reserva_vuelo_clase(numero, vuelo, fecha_vuelo, clase) VALUES(501, "0001", "2019/08/21", "Economica");
+INSERT INTO reserva_vuelo_clase(numero, vuelo, fecha_vuelo, clase) VALUES(502, "0001", "2019/08/21", "Economica");
+INSERT INTO reserva_vuelo_clase(numero, vuelo, fecha_vuelo, clase) VALUES(503, "0003", "2019/10/19", "Media");
+INSERT INTO reserva_vuelo_clase(numero, vuelo, fecha_vuelo, clase) VALUES(504, "0004", "2019/12/31", "Intermedia");
+INSERT INTO reserva_vuelo_clase(numero, vuelo, fecha_vuelo, clase) VALUES(505, "0005", "2020/02/01", "Economica");
+INSERT INTO reserva_vuelo_clase(numero, vuelo, fecha_vuelo, clase) VALUES(506, "0001", "2019/08/21", "Media");
+
---
+++ scripts/vuelos.sql
@@ -0,0 +1,308 @@
+# Base de datos Vuelos
+
+# Creacion de la base de datos
+CREATE DATABASE vuelos;
+USE vuelos;
+
+# --------------------------------------------
+#	
+#	Entidades
+# 
+
+CREATE TABLE pasajeros(
+	doc_tipo VARCHAR(10) NOT NULL,
+	doc_nro INT(10) UNSIGNED NOT NULL,
+	apellido VARCHAR(45) NOT NULL,
+	nombre VARCHAR(45) NOT NULL,
+	direccion VARCHAR(45) NOT NULL,
+	telefono VARCHAR(45) NOT NULL,
+	nacionalidad VARCHAR(45) NOT NULL,
+	
+	CONSTRAINT pk_pasajeros
+	PRIMARY KEY (doc_tipo, doc_nro)	
+)	ENGINE = InnoDB;
+
+CREATE TABLE empleados(
+	legajo INT(10) UNSIGNED NOT NULL,
+	password CHAR(32) NOT NULL,
+	doc_tipo VARCHAR(10) NOT NULL,
+	doc_nro INT(10) UNSIGNED NOT NULL,
+	apellido VARCHAR(45) NOT NULL,
+	nombre VARCHAR(45) NOT NULL,
+	direccion VARCHAR(45) NOT NULL,
+	telefono VARCHAR(45) NOT NULL,
+	
+	CONSTRAINT pk_empleados
+	PRIMARY KEY (legajo)	
+)	ENGINE = InnoDB;
+
+CREATE TABLE ubicaciones(
+	pais VARCHAR(45) NOT NULL,
+	estado VARCHAR(45) NOT NULL,
+	ciudad VARCHAR(45) NOT NULL,
+	huso INT CHECK (huso >= -12 AND huso<= 12) NOT NULL,
+	
+	CONSTRAINT pk_ubicaciones
+	PRIMARY KEY (pais, estado, ciudad)
+)	ENGINE = InnoDB;
+
+CREATE TABLE aeropuertos(	
+	codigo VARCHAR(45) NOT NULL,
+	nombre VARCHAR(45) NOT NULL,
+	telefono VARCHAR(45) NOT NULL,
+	direccion VARCHAR(45) NOT NULL,
+	pais VARCHAR(45) NOT NULL,
+	estado VARCHAR(45) NOT NULL,
+	ciudad VARCHAR(45) NOT NULL,
+	
+	CONSTRAINT pk_aeropuertos
+	PRIMARY KEY (codigo),
+	
+	CONSTRAINT fk_aeropuertos
+	FOREIGN KEY (pais, estado, ciudad) REFERENCES ubicaciones (pais, estado, ciudad)
+)	ENGINE = InnoDB;
+
+CREATE TABLE vuelos_programados(
+	numero VARCHAR(45) NOT NULL,
+	aeropuerto_salida VARCHAR(45) NOT NULL,
+	aeropuerto_llegada VARCHAR(45) NOT NULL,
+	
+	CONSTRAINT pk_vuelos_programados
+	PRIMARY KEY (numero),
+	
+	CONSTRAINT fk_vuelos_programados_aeropuerto_salida
+	FOREIGN KEY (aeropuerto_salida) references aeropuertos (codigo),
+	
+	CONSTRAINT fk_vuelos_programados_aeropuerto_llegada
+	FOREIGN KEY (aeropuerto_llegada) references aeropuertos (codigo)
+)	ENGINE = InnoDB;
+
+CREATE TABLE modelos_avion(
+	modelo VARCHAR(45) NOT NULL,
+	fabricante VARCHAR(45) NOT NULL,
+	cabinas INT UNSIGNED NOT NULL,
+	cant_asientos INT UNSIGNED NOT NULL,
+	
+	CONSTRAINT pk_modelos_avion
+	PRIMARY KEY (modelo)
+)	ENGINE = InnoDB;
+
+CREATE TABLE salidas(
+	vuelo VARCHAR(45) NOT NULL,
+	dia ENUM('Do','Lu','Ma','Mi','Ju','Vi','Sa') NOT NULL,
+	hora_sale TIME NOT NULL,
+	hora_llega TIME NOT NULL,
+	modelo_avion VARCHAR(45) NOT NULL,
+	
+	CONSTRAINT pk_salidas
+	PRIMARY KEY (vuelo, dia),
+	
+	CONSTRAINT fk_salidas_vuelo
+	FOREIGN KEY (vuelo) references vuelos_programados(numero),
+	
+	CONSTRAINT fk_salidas_modelo_avion
+	FOREIGN KEY (modelo_avion) references modelos_avion(modelo)
+) ENGINE = InnoDB;
+
+CREATE TABLE instancias_vuelo(
+	vuelo VARCHAR(45) NOT NULL,
+	fecha DATE NOT NULL,
+	
+	dia ENUM('Do','Lu','Ma','Mi','Ju','Vi','Sa') NOT NULL,
+	estado VARCHAR(45),	
+	
+	CONSTRAINT pk_instancias_vuelo
+	PRIMARY KEY (vuelo, fecha),
+	
+	CONSTRAINT fk_instancias_vuelo
+	FOREIGN KEY (vuelo, dia) references salidas(vuelo, dia)
+) ENGINE = InnoDB;
+
+CREATE TABLE clases(
+	nombre VARCHAR(45) NOT NULL,
+	porcentaje DECIMAL(2,2) unsigned  CHECK(porcentaje>=0 AND porcentaje<= 0.99) NOT NULL,
+	
+	CONSTRAINT pk_nombre
+	PRIMARY KEY (nombre)
+)	ENGINE = InnoDB;
+
+CREATE TABLE comodidades(
+	codigo INT UNSIGNED NOT NULL,
+	descripcion TEXT NOT NULL,
+	
+	CONSTRAINT pk_comodidades
+	PRIMARY KEY (codigo)
+)	ENGINE = InnoDB;
+
+CREATE TABLE reservas(
+	numero INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	fecha DATE NOT NULL,
+	vencimiento DATE NOT NULL,
+	estado VARCHAR(45) NOT NULL,
+	doc_tipo VARCHAR(10) NOT NULL,		
+	doc_nro INT(10) UNSIGNED NOT NULL,
+	legajo INT(10) UNSIGNED NOT NULL,
+	
+	CONSTRAINT pk_reservas
+	PRIMARY KEY (numero),
+	
+	CONSTRAINT fk_reservas_doc_tipo
+	FOREIGN KEY (doc_tipo, doc_nro) references pasajeros(doc_tipo, doc_nro),
+	
+	CONSTRAINT fk_reservas_legajo
+	FOREIGN KEY (legajo) references empleados(legajo)
+)	ENGINE = InnoDB;
+
+#
+#	Relaciones
+#
+
+CREATE TABLE brinda(
+	vuelo VARCHAR(45) NOT NULL,
+	dia ENUM('Do','Lu','Ma','Mi','Ju','Vi','Sa') NOT NULL,
+	clase VARCHAR(45) NOT NULL,
+	precio DECIMAL(7,2) UNSIGNED NOT NULL,
+	cant_asientos INT UNSIGNED NOT NULL,
+	
+	CONSTRAINT pk_brinda
+	PRIMARY KEY (vuelo, dia, clase),
+	
+	CONSTRAINT fk_brinda_vuelo_dia
+	FOREIGN KEY (vuelo, dia) references salidas(vuelo, dia),
+	
+	CONSTRAINT fk_brinda_clase
+	FOREIGN KEY (clase) references clases(nombre)
+)	ENGINE = InnoDB;
+
+CREATE TABLE posee(
+	clase VARCHAR(45) NOT NULL,	
+	comodidad INT UNSIGNED NOT NULL,
+	
+	CONSTRAINT pk_posee
+	PRIMARY KEY (clase, comodidad),
+	
+	CONSTRAINT fk_posee_clase
+	FOREIGN KEY (clase) references clases(nombre),
+	
+	CONSTRAINT fk_posee_comodidad
+	FOREIGN KEY (comodidad) references comodidades(codigo)
+) ENGINE = InnoDB;
+
+CREATE TABLE reserva_vuelo_clase(
+	numero INT UNSIGNED NOT NULL,
+	vuelo VARCHAR(45) NOT NULL,
+	fecha_vuelo DATE NOT NULL,		
+	clase VARCHAR(45) NOT NULL,
+
+	CONSTRAINT pk_reserva_vuelo_clase
+	PRIMARY KEY (numero, vuelo, fecha_vuelo),
+	
+	CONSTRAINT fk_reserva_vuelo_clase_numero
+	FOREIGN KEY (numero) references reservas(numero),
+	
+	CONSTRAINT fk_reserva_vuelo_clase_fecha_vuelo
+	FOREIGN KEY (vuelo, fecha_vuelo) references instancias_vuelo(vuelo, fecha),
+	 
+	CONSTRAINT fk_reserva_vuelo_clase
+	FOREIGN KEY (clase) references clases(nombre)
+) ENGINE = InnoDB;
+
+#
+#	Creacion de vistas
+#
+
+CREATE VIEW datos AS
+	SELECT 	*
+	FROM  	((((((vuelos_programados as v 
+			join (select	codigo as cod_llegada,
+							nombre as nombre_llegada, 
+							ciudad as ciudad_llegada, 
+							estado as estado_llegada, 
+							pais as pais_llegada 
+							from aeropuertos) as t1
+			on v.aeropuerto_llegada=t1.cod_llegada)
+			
+			
+			
+			join (select	codigo as cod_salida, 
+							nombre as nombre_salida, 
+							ciudad as ciudad_salida,
+							estado as estado_salida,
+							pais as pais_salida
+							from aeropuertos) as t2
+			on v.aeropuerto_salida=t2.cod_salida)
+			
+			join (select	hora_sale, 
+							hora_llega,	
+							modelo_avion,
+							dia as dia_salida,
+							vuelo as vuelo_salida
+							from salidas) as t3
+			on v.numero = t3.vuelo_salida)
+			
+			join (brinda t4)
+			on t3.vuelo_salida = t4.vuelo and t3.dia_salida = t4.dia)
+			
+			join (select 	fecha,
+							vuelo as vuelo_iv,
+							dia as dia_iv
+							from instancias_vuelo) as t5
+			on t4.vuelo = t5.vuelo_iv and t4.dia = t5.dia_iv)
+			
+			join (select 	porcentaje as porcentaje_clase,
+							nombre as nombre_clase_brinda
+							from clases) as t6
+			on clase = t6.nombre_clase_brinda);
+									
+		
+CREATE VIEW vuelos_disponibles AS
+	SELECT  vuelo,
+			precio,
+			porcentaje_clase,
+			pais_salida,
+			pais_llegada,
+			nombre_salida,
+			nombre_llegada,
+			modelo_avion,
+			hora_sale,
+			hora_llega,
+			fecha,
+			estado_salida,
+			estado_llegada,
+			dia,
+			cod_salida,
+			cod_llegada,
+			clase,
+			ciudad_salida,
+			ciudad_llegada,
+			cant_asientos,
+			aeropuerto_salida,
+			aeropuerto_llegada,
+			round(((cant_asientos + cant_asientos*porcentaje_clase) - count(numero_reserva)), 0) as cant_libres,
+			if (hora_llega > hora_sale, 
+				timediff(hora_llega,hora_sale), SEC_TO_TIME (TIME_TO_SEC(hora_llega) + TIME_TO_SEC(TIMEDIFF('24:00:00',hora_sale)))      
+			) as tiempo_estimado
+	FROM datos natural left join (select vuelo,	fecha_vuelo as fecha, clase, numero as numero_reserva from reserva_vuelo_clase) as t1
+	GROUP BY vuelo, fecha, clase;
+
+#
+#	Creacion de usuarios y privilegios
+#
+
+CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin';
+GRANT ALL PRIVILEGES ON vuelos.* TO 'admin'@'localhost' WITH GRANT OPTION;
+
+
+CREATE USER 'empleado'@'%' IDENTIFIED BY 'empleado';
+GRANT SELECT ON vuelos.* TO 'empleado'@'%';
+GRANT DELETE, INSERT, UPDATE ON vuelos.reservas TO 'empleado'@'%';
+GRANT DELETE, INSERT, UPDATE ON vuelos.pasajeros TO 'empleado'@'%';
+GRANT DELETE, INSERT, UPDATE ON vuelos.reserva_vuelo_clase TO 'empleado'@'%';
+
+
+CREATE USER 'cliente'@'%' IDENTIFIED BY 'cliente';
+GRANT SELECT ON vuelos.vuelos_disponibles TO 'cliente'@'%';
+
+
+
+
