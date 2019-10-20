dce9cf872858fd2048806511f21f6c3daf40d7b4
guidoChia <chiaguido@gmail.com>
2019-10-20 18:08:25 -0300

    TRIGGER FINISHED

--- scripts/trigger.sql
+++ scripts/trigger.sql
@@ -1,24 +1,24 @@
-USE vuelos;
-
-WITH aux AS 
-(select selected_date from 
+/*CREATE TABLE vuelos.aux
+(select selected_date, elt(dayofweek(selected_date), 'Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sa') AS dia from 
 (select t1.i AS aux, adddate(curdate(), t2.i*100 + t1.i*10 + t0.i) selected_date from
  (select 0 i union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t0,
  (select 0 i union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t1,
  (select 0 i union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t2) v
-where selected_date between curdate() and date_add(curdate(), INTERVAL 1 YEAR))
+where selected_date between curdate() and date_add(curdate(), INTERVAL 1 YEAR));*/
 
-SELECT * FROM aux;
+CREATE TRIGGER vuelos.trigger_vuelos
+AFTER INSERT ON vuelos.salidas
+FOR EACH ROW 
+	INSERT INTO vuelos.instancias_vuelo(vuelo, fecha, dia, estado) 
+	SELECT vuelo, selected_date AS fecha, dia, 'a tiempo' 
+	FROM (SELECT * FROM salidas WHERE vuelo = NEW.vuelo) AS t1 NATURAL JOIN (select selected_date, elt(dayofweek(selected_date), 'Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sa') AS dia from 
+																			(select t1.i AS aux, adddate(curdate(), t2.i*100 + t1.i*10 + t0.i) selected_date from
+																			(select 0 i union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t0,
+																			(select 0 i union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t1,
+																			(select 0 i union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t2) v
+																			where selected_date between curdate() and date_add(curdate(), INTERVAL 1 YEAR))	 AS t2		
+;
 
-
-/*CREATE TRIGGER trigger_vuelos
-ON salidas
-AFTER INSERT 
-AS
-BEGIN
-	INSERT INTO instancias_vuelo(vuelo, fecha, dia, estado)
-	SELECT vuelo, fecha, 
-END*/
 /*WITH aux_table AS (
 		SELECT curdate() AS from_date
 		UNION ALL
